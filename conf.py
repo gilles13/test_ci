bc3cbe38a4ebc4efc91da7cf0f28bca852dfdaa7
Gilles Fidani <gillesfidani@gmail.com>
2021-09-12 17:09:33 +0200

    ajout infos persos gh/twitter

--- conf.py
+++ conf.py
@@ -1,3 +1,4 @@
+# GENERAL CONF
 extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.githubpages',
@@ -11,3 +12,17 @@ nbsphinx_allow_errors = True
 
 # Add type of source files
 source_suffix = ['.rst', '.md', '.ipynb']
+
+# OPTIONS FOR HTML OUTPUT
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'pydata_sphinx_theme'
+
+html_theme_options = {
+        "github_url": "https://github.com/gilles13/",
+        "twitter_url": "https://twitter.com/gillesfidani",
+        "search_bar_text": "Search this site...",
+        "navbar_end": ["search-field.html", "navbar-icon-links"],
+        }
